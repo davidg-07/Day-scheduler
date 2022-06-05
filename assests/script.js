af95f9648351a62b7024b955979e63f8c98b575c
David Galvez <david.dmg17@gmail.com>
2022-06-05 07:57:42 -0700

    adding time slots

---
+++ assests/script.js
@@ -0,0 +1,70 @@
+function getHeaderDate() {
+    var updateCurrentHeaderDate = moment().format('dddd, MMMM Do');
+    $("#currentDay").text(updateCurrentHeaderDate);
+}
+
+getHeaderDate();
+
+  var planner = [
+    {
+        id: "a",
+        hour: "09:00",
+        time: "09",
+        activity: ""
+    },
+    {
+        id: "b",
+        hour: "10:00",
+        time: "10",
+        activity: ""
+    },
+    {
+        id: "c",
+        hour: "11:00",
+        time: "11",
+        activity: ""
+    },
+    {
+        id: "d",
+        hour: "12:00",
+        time: "12",
+        activity: ""
+    },
+    {
+        id: "e",
+        hour: "13:00",
+        time: "13",
+        activity: ""
+    },
+    {
+        id: "f",
+        hour: "14:00",
+        time: "14",
+        activity: ""
+    },
+    {
+        id: "g",
+        hour: "15:00",
+        time: "15",
+        activity: ""
+    },
+    {
+        id: "h",
+        hour: "16:00",
+        time: "16",
+        activity: ""
+    },
+    {
+        id: "i",
+        hour: "17:00",
+        time: "17",
+        activity: ""
+    },
+    {
+        id: "j",
+        hour: "18:00",
+        time: "18",
+        activity: ""
+    },
+    
+]
