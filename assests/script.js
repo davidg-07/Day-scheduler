8623bdab27f996b054853bd1af6cb4c266ec5463
David Galvez <david.dmg17@gmail.com>
2022-06-09 21:02:31 -0700

    time blocks will go into local storage

--- assests/script.js
+++ assests/script.js
@@ -11,6 +11,17 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     });
     
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
     function hourTracker() {
         //get current number of hours.
         var currentHour = moment().hour(); 
