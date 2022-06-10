ef75b7916aec25c15136b2a33bb41854dd387417
David Galvez <david.dmg17@gmail.com>
2022-06-09 21:04:31 -0700

    time blocks will show past or present

--- assests/script.js
+++ assests/script.js
@@ -26,5 +26,27 @@ $(document).ready(function () {
         //get current number of hours.
         var currentHour = moment().hour(); 
         // use of moment.js
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+
+            //check if we've moved past this time, click into css/html given classes of past, present, or future
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
     }
+    hourTracker(); 
 });
